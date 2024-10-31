a2d3b1785aaa7d8988af65c8461d64468de7e9b0
johaneris <jsavalos@uamv.edu.ni>
2024-10-31 17:37:47 -0600

    Medio llevo la interfaz

--- interfazdeusuario/InterfazdeUsuario/InterfazdeUsuario/Form1.Designer.cs
+++ interfazdeusuario/InterfazdeUsuario/InterfazdeUsuario/Form1.Designer.cs
@@ -29,12 +29,232 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.btnAbout = new FontAwesome.Sharp.IconButton();
+            this.btnAyuda = new FontAwesome.Sharp.IconButton();
+            this.btnAdministrador = new FontAwesome.Sharp.IconButton();
+            this.btnMiembros = new FontAwesome.Sharp.IconButton();
+            this.PnlLogo = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pnlMenu.SuspendLayout();
+            this.PnlLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(51)))), ((int)(((byte)(59)))));
+            this.pnlMenu.Controls.Add(this.btnAbout);
+            this.pnlMenu.Controls.Add(this.btnAyuda);
+            this.pnlMenu.Controls.Add(this.btnAdministrador);
+            this.pnlMenu.Controls.Add(this.btnMiembros);
+            this.pnlMenu.Controls.Add(this.PnlLogo);
+            this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlMenu.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(51)))), ((int)(((byte)(59)))));
+            this.pnlMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenu.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(344, 647);
+            this.pnlMenu.TabIndex = 3;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAbout.FlatAppearance.BorderSize = 0;
+            this.btnAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAbout.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbout.ForeColor = System.Drawing.Color.White;
+            this.btnAbout.IconChar = FontAwesome.Sharp.IconChar.UserGroup;
+            this.btnAbout.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(152)))), ((int)(((byte)(143)))));
+            this.btnAbout.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnAbout.IconSize = 50;
+            this.btnAbout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAbout.Location = new System.Drawing.Point(0, 570);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnAbout.Size = new System.Drawing.Size(344, 111);
+            this.btnAbout.TabIndex = 4;
+            this.btnAbout.Text = "Acerca de Gymtrack";
+            this.btnAbout.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click_1);
+            this.btnAbout.MouseLeave += new System.EventHandler(this.btnAbout_MouseLeave);
+            this.btnAbout.MouseHover += new System.EventHandler(this.btnAbout_MouseHover);
+            // 
+            // btnAyuda
+            // 
+            this.btnAyuda.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAyuda.FlatAppearance.BorderSize = 0;
+            this.btnAyuda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAyuda.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAyuda.ForeColor = System.Drawing.Color.White;
+            this.btnAyuda.IconChar = FontAwesome.Sharp.IconChar.Question;
+            this.btnAyuda.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(152)))), ((int)(((byte)(143)))));
+            this.btnAyuda.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnAyuda.IconSize = 50;
+            this.btnAyuda.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAyuda.Location = new System.Drawing.Point(0, 459);
+            this.btnAyuda.Name = "btnAyuda";
+            this.btnAyuda.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnAyuda.Size = new System.Drawing.Size(344, 111);
+            this.btnAyuda.TabIndex = 3;
+            this.btnAyuda.Text = "Ayuda";
+            this.btnAyuda.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAyuda.UseVisualStyleBackColor = true;
+            this.btnAyuda.Click += new System.EventHandler(this.btnAyuda_Click_1);
+            this.btnAyuda.MouseLeave += new System.EventHandler(this.btnAyuda_MouseLeave);
+            this.btnAyuda.MouseHover += new System.EventHandler(this.btnAyuda_MouseHover);
+            // 
+            // btnAdministrador
+            // 
+            this.btnAdministrador.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAdministrador.FlatAppearance.BorderSize = 0;
+            this.btnAdministrador.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdministrador.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdministrador.ForeColor = System.Drawing.Color.White;
+            this.btnAdministrador.IconChar = FontAwesome.Sharp.IconChar.AddressCard;
+            this.btnAdministrador.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(152)))), ((int)(((byte)(143)))));
+            this.btnAdministrador.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnAdministrador.IconSize = 50;
+            this.btnAdministrador.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdministrador.Location = new System.Drawing.Point(0, 348);
+            this.btnAdministrador.Name = "btnAdministrador";
+            this.btnAdministrador.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnAdministrador.Size = new System.Drawing.Size(344, 111);
+            this.btnAdministrador.TabIndex = 2;
+            this.btnAdministrador.Text = "Administrador";
+            this.btnAdministrador.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAdministrador.UseVisualStyleBackColor = true;
+            this.btnAdministrador.MouseLeave += new System.EventHandler(this.btnAdministrador_MouseLeave);
+            this.btnAdministrador.MouseHover += new System.EventHandler(this.btnAdministrador_MouseHover);
+            // 
+            // btnMiembros
+            // 
+            this.btnMiembros.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMiembros.FlatAppearance.BorderSize = 0;
+            this.btnMiembros.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMiembros.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMiembros.ForeColor = System.Drawing.Color.White;
+            this.btnMiembros.IconChar = FontAwesome.Sharp.IconChar.Dumbbell;
+            this.btnMiembros.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(152)))), ((int)(((byte)(143)))));
+            this.btnMiembros.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMiembros.IconSize = 50;
+            this.btnMiembros.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMiembros.Location = new System.Drawing.Point(0, 237);
+            this.btnMiembros.Name = "btnMiembros";
+            this.btnMiembros.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnMiembros.Size = new System.Drawing.Size(344, 111);
+            this.btnMiembros.TabIndex = 1;
+            this.btnMiembros.Text = "Miembros";
+            this.btnMiembros.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMiembros.UseVisualStyleBackColor = true;
+            this.btnMiembros.MouseLeave += new System.EventHandler(this.btnMiembros_MouseLeave);
+            this.btnMiembros.MouseHover += new System.EventHandler(this.btnMiembros_MouseHover);
+            // 
+            // PnlLogo
+            // 
+            this.PnlLogo.Controls.Add(this.pictureBox1);
+            this.PnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.PnlLogo.Margin = new System.Windows.Forms.Padding(2);
+            this.PnlLogo.Name = "PnlLogo";
+            this.PnlLogo.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.PnlLogo.Size = new System.Drawing.Size(344, 237);
+            this.PnlLogo.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::InterfazdeUsuario.Properties.Resources.x;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(341, 303);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(51)))), ((int)(((byte)(59)))));
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(344, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(856, 70);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::InterfazdeUsuario.Properties.Resources.logo_uam_2;
+            this.pictureBox2.Location = new System.Drawing.Point(15, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(120, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(43)))), ((int)(((byte)(50)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(344, 70);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(856, 10);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(51)))), ((int)(((byte)(59)))));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(344, 80);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(856, 567);
+            this.panel3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1200, 647);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlMenu);
+            this.Font = new System.Drawing.Font("Calisto MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pnlMenu.ResumeLayout(false);
+            this.PnlLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.Panel PnlLogo;
+        private FontAwesome.Sharp.IconButton btnMiembros;
+        private FontAwesome.Sharp.IconButton btnAbout;
+        private FontAwesome.Sharp.IconButton btnAyuda;
+        private FontAwesome.Sharp.IconButton btnAdministrador;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
